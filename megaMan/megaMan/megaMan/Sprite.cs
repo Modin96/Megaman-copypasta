2b584e75a892f2d587b01e6d4e57a11ee0d201ec
Gaeddar Cassius <ingenbryrsigomdenjavlaemailen@skitpadig.nu>
2015-01-27 13:08:23 +0100

    sad
    sda

--- megaMan/megaMan/megaMan/Sprite.cs
+++ megaMan/megaMan/megaMan/Sprite.cs
@@ -19,47 +19,16 @@ namespace megaMan
         public Texture2D texture;
         public Vector2 position;
 
-<<<<<<< HEAD
+
        public Sprite(Texture2D texture, Vector2 position)
        {
            this.texture = texture;
            this.position = position;
-           this.speed = 4.5f;
+           
        }
-   
-       public void Draw(SpriteBatch spriteBatch)
-       {
-           Vector2 tmp;
-           tmp.X = position.X;
-           tmp.Y = position.Y;
-           spriteBatch.Draw(texture,tmp, Color.White);
-       }
+        
+        public bool DetectCollision(Rectangle otherRect) // kod för kollision
 
-
-       public void MoveRight() // ökar x värdet (åker höger)
-       {
-           position.X -= speed;
-       }
-
-       public void MoveLeft() // minskar x värdet (åker vänster)
-       {
-           position.X += speed;
-       }
-
-       public void MoveUp() // ökar y värdet (åker upp)
-       {
-           position.Y -= speed;
-       }
-
-       public void MoveDown() // minskar y värdet (åker ner)
-       {
-           position.Y += speed;
-       }
-
-
-
-
-       public bool DetectCollision(Rectangle otherRect) // kod för kollision
         {
             Rectangle myRect = new Rectangle(Convert.ToInt32(position.X), Convert.ToInt32(position.Y), texture.Width, texture.Height );
             
@@ -69,7 +38,6 @@ namespace megaMan
             }
             return false;
         }
-=======
->>>>>>> origin/master
+
     }
 }
